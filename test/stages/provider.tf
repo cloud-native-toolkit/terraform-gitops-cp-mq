8be5ecf29b725074164e6058d2496fc8fc31ae87
balasgit <bala@us.ibm.com>
2022-09-27 17:06:21 -0500

    Fix provider.tf

--- test/stages/provider.tf
+++ test/stages/provider.tf
@@ -3,3 +3,7 @@ provider "gitops" {
     token = var.git_token
     bin_dir = module.setup_clis.bin_dir
 }
+
+module setup_clis {
+     source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+}
